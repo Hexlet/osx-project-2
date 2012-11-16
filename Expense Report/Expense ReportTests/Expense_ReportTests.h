67d058aadaf365244476df4874002b6115aa2934
weitek <weitek.mvl@gmail.com>
2012-11-16 20:21:49 +0400

    Clear

---
+++ Expense Report/Expense ReportTests/Expense_ReportTests.h
@@ -0,0 +1,13 @@
+//
+//  Expense_ReportTests.h
+//  Expense ReportTests
+//
+//  Created by Victor Mylcin on 16.11.12.
+//  Copyright (c) 2012 Victor Mylcin. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Expense_ReportTests : SenTestCase
+
+@end
---
+++ Expense Report/Expense ReportTests/Expense_ReportTests.m
@@ -0,0 +1,32 @@
+//
+//  Expense_ReportTests.m
+//  Expense ReportTests
+//
+//  Created by Victor Mylcin on 16.11.12.
+//  Copyright (c) 2012 Victor Mylcin. All rights reserved.
+//
+
+#import "Expense_ReportTests.h"
+
+@implementation Expense_ReportTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Expense ReportTests");
+}
+
+@end
