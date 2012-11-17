ed068211426cce7ff6b9cb23abf73229ed2e6ff9
bj <big_jump@mail.ru>
2012-11-17 22:50:44 +0400

    empty

---
+++ OtherTwitter/OtherTwitter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OtherTwitter
+//
+//  Created by bj on 17.11.12.
+//  Copyright (c) 2012 bj. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ OtherTwitter/OtherTwitter/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  OtherTwitter
+//
+//  Created by bj on 17.11.12.
+//  Copyright (c) 2012 bj. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
