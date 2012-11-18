78065da350570ed76412502e78827438974256e1
iGR <redchuk@gmail.com>
2012-11-18 17:32:06 +0200

    Created project with a window. Options checkbox group added.

---
+++ PowerRegexTester/PowerRegexTester/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PowerRegexTester
+//
+//  Created by Igor on 18/11/2012.
+//  Copyright (c) 2012 Igor Redchuk. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (weak) IBOutlet NSMatrix *optionsMatrix;
+
+@end
---
+++ PowerRegexTester/PowerRegexTester/AppDelegate.m
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.m
+//  PowerRegexTester
+//
+//  Created by Igor on 18/11/2012.
+//  Copyright (c) 2012 Igor Redchuk. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+
+}
+
+@end
