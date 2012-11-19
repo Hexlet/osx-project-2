640cf7ff8d8da6a24f219b1b914b8cabf362ee24
Alex Ovseyenko <mixtline@gmail.com>
2012-11-19 21:54:02 +0400

    FINAL

---
+++ WordCards/WordCards/Card2WindowController.m
@@ -0,0 +1,34 @@
+//
+//  Card2WindowController.m
+//  WordCards
+//
+//  Created by alex on 19/11/2012.
+//  Copyright (c) 2012 alex. All rights reserved.
+//
+
+#import "Card2WindowController.h"
+
+@interface Card2WindowController ()
+
+@end
+
+@implementation Card2WindowController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
