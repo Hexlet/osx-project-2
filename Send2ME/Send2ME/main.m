fff48fabe49a0f278af1182b8e70ddcf244ae102
Сергей <admin@finesites.ru>
2012-11-20 22:46:04 +0400

    Идея приложения.
    Идея приложения. Предварительный набросок функционала.

---
+++ Send2ME/Send2ME/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Send2ME
+//
+//  Created by admin on 11/20/12.
+//  Copyright (c) 2012 d1sc0nnect0r. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
