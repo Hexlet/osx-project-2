b4317d8d75f43843a98ee5bede174218e0a14710
ed <edsakovich@me.com>
2012-11-21 00:39:34 +0400

    Добавлен файл README

---
+++ MiniDent osx-project-2/MiniDent osx-project-2/EDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EDAppDelegate.h
+//  MiniDent osx-project-2
+//
+//  Created by ed on 20.11.12.
+//  Copyright (c) 2012 ed. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface EDAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
