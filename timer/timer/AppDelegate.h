0c591ed4cb36353fa049b1a61267b9c3e754e476
maxakeheomax@mail.ru <25662566m>
2012-11-20 16:49:06 +0400

    проект 2

---
+++ timer/timer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  timer
+//
+//  Created by Максим on 20.11.12.
+//  Copyright (c) 2012 CarelessApps. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
