240e5ad935056b1fd7412cac61065d0856ccda8f
Dmitry Volevodz <dimavolevodz@gmail.com>
2012-11-22 06:26:50 +0400

    0.1 Added interface and read me.md

---
+++ Website blocker/Website blocker/DVGAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  DVGAppDelegate.m
+//  Website blocker
+//
+//  Created by Dmitry Volevodz on 22.11.12.
+//  Copyright (c) 2012 Dmitry Volevodz. All rights reserved.
+//
+
+#import "DVGAppDelegate.h"
+
+@implementation DVGAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ Website blocker/Website blocker/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Website blocker
+//
+//  Created by Dmitry Volevodz on 22.11.12.
+//  Copyright (c) 2012 Dmitry Volevodz. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
