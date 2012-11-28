36496d5b8ca1e222c736bc8c0dc8651f7e33331e
Victorya <vchigirova@gmail.com>
2012-11-28 23:05:20 +0400

    first version of UI

---
+++ PaymentsAndExpensesOrganiser/PaymentsAndExpensesOrganiser/OrganizerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OrganizerAppDelegate.h
+//  PaymentsAndExpensesOrganiser
+//
+//  Created by Eugene on 11/28/12.
+//  Copyright (c) 2012 Victorya. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface OrganizerAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ PaymentsAndExpensesOrganiser/PaymentsAndExpensesOrganiser/OrganizerAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  OrganizerAppDelegate.m
+//  PaymentsAndExpensesOrganiser
+//
+//  Created by Eugene on 11/28/12.
+//  Copyright (c) 2012 Victorya. All rights reserved.
+//
+
+#import "OrganizerAppDelegate.h"
+
+@implementation OrganizerAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
