09cb1c03fd2914f13e87555b3859eb063e279048
Admin <Admin@Admins-Mac.local>
2012-11-30 16:55:00 +0200

    initial commit

---
+++ ProximityDating/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ProximityDating
+//
+//  Created by Admin on 11/30/12.
+//  Copyright (c) 2012 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ProximityDating/DataViewController.m
@@ -0,0 +1,35 @@
+//
+//  DataViewController.m
+//  ProximityDating
+//
+//  Created by Admin on 11/30/12.
+//  Copyright (c) 2012 Admin. All rights reserved.
+//
+
+#import "DataViewController.h"
+
+@interface DataViewController ()
+
+@end
+
+@implementation DataViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    self.dataLabel.text = [self.dataObject description];
+}
+
+@end
