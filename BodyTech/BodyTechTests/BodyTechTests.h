57bc7ffd90fdcc90f031e3acfb0f96478b760741
Dmitry Davidov <dmitrii.davidov@gmail.com>
2012-12-02 20:02:51 +0400

    Реализован интерфейс добавления и удаления упражнений. Для наглядности посредством binding'а добавлены тестовые данные.

---
+++ BodyTech/BodyTechTests/BodyTechTests.h
@@ -0,0 +1,13 @@
+//
+//  BodyTechTests.h
+//  BodyTechTests
+//
+//  Created by Dmitry Davidov on 02.12.12.
+//  Copyright (c) 2012 Dmitry Davidov. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface BodyTechTests : SenTestCase
+
+@end
---
+++ BodyTech/BodyTechTests/BodyTechTests.m
@@ -0,0 +1,32 @@
+//
+//  BodyTechTests.m
+//  BodyTechTests
+//
+//  Created by Dmitry Davidov on 02.12.12.
+//  Copyright (c) 2012 Dmitry Davidov. All rights reserved.
+//
+
+#import "BodyTechTests.h"
+
+@implementation BodyTechTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in BodyTechTests");
+}
+
+@end
