d1b44768a619014a547a4f0d2a2bf0e1d9cd86fd
Michael Krinitsky <mbrdancer@gmail.com>
2012-12-02 17:52:52 +0400

    1st step - description & interface

---
+++ SixMinds/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SixMinds
+//
+//  Created by Michael Krinitsky on 12/2/12.
+//  Copyright (c) 2012 Michael Krinitsky. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
