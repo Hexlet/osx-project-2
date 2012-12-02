2c5695d31a31f6c42c5baf4062de91415ddae8d5
macw <student023@gmail.com>
2012-12-02 16:42:19 +0400

    Начал интерфейс сервера

---
+++ SnakeGame/SnakeGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SnakeGame
+//
+//  Created by mac205-2 on 02.12.12.
+//  Copyright (c) 2012 zhorkov023. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ SnakeGame/SnakeGame/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  SnakeGame
+//
+//  Created by mac205-2 on 02.12.12.
+//  Copyright (c) 2012 zhorkov023. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
