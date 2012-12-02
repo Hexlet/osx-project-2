042c8b592897acfa21f84346ebdf095b139278fa
Jazz2 <jazz4all@rambler.ru>
2012-12-02 12:11:50 +0200

    User interface

---
+++ Alpha/Alpha/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Alpha
+//
+//  Created by Eraser on 12/2/12.
+//  Copyright (c) 2012 Eraser. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Alpha/Alpha/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Alpha
+//
+//  Created by Eraser on 12/2/12.
+//  Copyright (c) 2012 Eraser. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
