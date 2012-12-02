5d2ca5136d88b2ae4c98c172ff011afbb6088e31
AlloyDev <alloy.stas@gmail.com>
2012-12-02 21:59:53 +0400

    Done.

---
+++ BackgroundMasta/BackgroundMasta/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  BackgroundMasta
+//
+//  Created by Tsyganov Stanislav on 02.12.12.
+//  Copyright (c) 2012 Tsyganov Stanislav. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSView *mainView;
+
+@end
