8c6e5c6af0f3693fa5a4684e852dc8c6452f7ea9
Kirill Gorshkov <kan.kiryan@gmail.com>
2012-12-03 20:35:56 +0400

    Revert "[ADD] description, comments, NSString==nil error fix"
    This reverts commit ca9ac3afcc7f4a45c5ca402fd737fbaf92b57e3f.

--- osx-project-2/AppDelegate.m
+++ osx-project-2/AppDelegate.m
@@ -20,14 +20,9 @@
     timer = [NSTimer scheduledTimerWithTimeInterval:0.01f target:self selector:@selector(onTick) userInfo:nil repeats:YES];
 }
 
-///
-/// Вызывается таймером, при несовпадении copyCount и текущего числа изменений буфера перезаписывает
-/// copyCount и вызывает copy
-///
 -(void) onTick {
     if (copyCount != [mainPasteboard changeCount]) {
         copyCount = [mainPasteboard changeCount];
-        /// для предотвращения коллизий при изменении буфера функцией pasteNow
         if (pasteboardSemaphore) {
             [self copy];
         }
@@ -36,19 +31,9 @@
         }
     }
 }
-
-///
-/// Вытаскивает строку из буфера обмена и записывает ее в pasteboardContainer,
-/// а ее укороченное представление - в ComboBox
-///
 - (void)copy {
-    NSString *pasteboardString;
-    /// для предотвращения ошибок типа NSString == nil
-    while (!pasteboardString) {
-         pasteboardString = [mainPasteboard stringForType:NSStringPboardType];
-    }
+    NSString *pasteboardString = [mainPasteboard stringForType:NSStringPboardType];
     NSString *comboString;
-    /// проверяем, есть ли уже такая строка
     BOOL copyFlag = YES;
     for (int i = 0; i < [pasteboardContainer count]; i++) {
         if ([pasteboardString isEqualToString:[pasteboardContainer objectAtIndex:i]]) {
@@ -56,7 +41,6 @@
             break;
         }
     }
-    /// собираем строку для отображения в comboBox
     if ([pasteboardString length] > 50)
         comboString = [[[pasteboardString stringByReplacingOccurrencesOfString:@"\n" withString: @" "] substringToIndex:47] stringByAppendingString:@"..."];
     else
@@ -69,9 +53,6 @@
     [_comboBox setStringValue:comboString];
 }
 
-///
-/// Записывает текущую строку из ComboBox в буфер обмена
-///
 - (IBAction)pasteNow:(id)sender {
     pasteboardSemaphore = NO;
     [mainPasteboard declareTypes:[NSArray arrayWithObject:NSStringPboardType] owner:nil];
