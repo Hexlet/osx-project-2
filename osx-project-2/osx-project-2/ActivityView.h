c840855ecb662f46df66cbaae1e7f2a14192e46d
Pavel Popchikovsky <ppopchikovsky@gmail.com>
2012-12-04 02:17:38 +0400

    added ActivityView with simple OpenGL drawing

---
+++ osx-project-2/osx-project-2/ActivityView.h
@@ -0,0 +1,13 @@
+//
+//  ActivityView.h
+//  osx-project-2
+//
+//  Created by Pavel Popchikovsky on 04.12.2012.
+//  Copyright (c) 2012 Pavel Popchikovsky. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ActivityView : NSOpenGLView
+
+@end
