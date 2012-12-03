e3d27a01a8c09f5cd4a4ccf32822dd781ba7d76e
Arseniy <arseniy@zuev.by>
2012-12-04 01:39:28 +0300

    StudentHelper app

---
+++ university/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  university
+//
+//  Created by Arseniy on 12/3/12.
+//  Copyright (c) 2012 Arseniy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ university/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  university
+//
+//  Created by Arseniy on 12/3/12.
+//  Copyright (c) 2012 Arseniy. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
