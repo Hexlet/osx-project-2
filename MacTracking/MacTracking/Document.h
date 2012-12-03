0a175d66e7d28026f116ed52749051406c9e8866
iyulan <iyulan@rambler.ru>
2012-12-03 21:33:29 +0600

    Черновой вариант

---
+++ MacTracking/MacTracking/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  MacTracking
+//
+//  Created by eldar on 03.12.12.
+//  Copyright (c) 2012 eldar. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+@end
