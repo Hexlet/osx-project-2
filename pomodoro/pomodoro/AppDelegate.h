a1068785beabb0b447b346db40012d2e8cc7d418
Konstantin Kalbazov <kalbazov@gmail.com>
2012-12-04 00:03:23 +0400

    Added project files

---
+++ pomodoro/pomodoro/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  pomodoro
+//
+//  Created by Kote on 12/4/12.
+//  Copyright (c) 2012 Kote. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ pomodoro/pomodoro/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  pomodoro
+//
+//  Created by Kote on 12/4/12.
+//  Copyright (c) 2012 Kote. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
