54fcc6fd215d3f2e86a646e157f6f62fb1f6e750
Екатерина Полищук <veda-dia@ya.ru>
2012-12-04 02:19:48 +0400

    osx-project-2 - MyCookbook
    Sole

---
+++ MyCookbook/VVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VVAppDelegate.h
+//  MyCookbook
+//
+//  Created by Екатерина Полищук on 04.12.12.
+//  Copyright (c) 2012 Екатерина Полищук. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface VVAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ MyCookbook/VVAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  VVAppDelegate.m
+//  MyCookbook
+//
+//  Created by Екатерина Полищук on 04.12.12.
+//  Copyright (c) 2012 Екатерина Полищук. All rights reserved.
+//
+
+#import "VVAppDelegate.h"
+
+@implementation VVAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
