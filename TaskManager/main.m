b826772a33c4ec34f256be5f1869807ba28eec03
Pavel <pvl.matvienko@gmail.com>
2012-12-04 01:09:01 +0200

    Project 2
    Описание проекта в файле Project_Overview.pdf

---
+++ TaskManager/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TaskManager
+//
+//  Created by Павел Матвиенко on 29.11.12.
+//  Copyright (c) 2012 MaMa Creative Studio. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
