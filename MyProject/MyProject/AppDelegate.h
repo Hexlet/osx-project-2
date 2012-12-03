cb5d0270703ef2b29048e3528b848d0f2663f6fe
macedonez <macedonez@bk.ru>
2012-12-03 17:51:09 +0200

    Add Project

---
+++ MyProject/MyProject/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  MyProject
+//
+//  Created by Alexander on 03.12.12.
+//  Copyright (c) 2012 Alexander. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
