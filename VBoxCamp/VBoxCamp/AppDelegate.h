55d35390aaed2f4d3133d9dc88415206d5ba2805
Dmitry Zavorokhin <goodman116@gmail.com>
2012-12-03 21:02:09 +0400

    Initial commit

---
+++ VBoxCamp/VBoxCamp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VBoxCamp
+//
+//  Created by Dmitriy Zavorokhin on 12/3/12.
+//  Copyright (c) 2012 goodman116@gmail.com. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ VBoxCamp/VBoxCamp/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  VBoxCamp
+//
+//  Created by Dmitriy Zavorokhin on 12/3/12.
+//  Copyright (c) 2012 goodman116@gmail.com. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
