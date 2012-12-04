c80536a7e5dd4a2d40cfab8ee1e5bbb4314bd3fb
fedoroff <administrator@Administrators-Mac.local>
2012-12-04 18:14:56 +0200

    1

---
+++ Just Another Smart Renamer/Just Another Smart RenamerTests/Just_Another_Smart_RenamerTests.m
@@ -0,0 +1,32 @@
+//
+//  Just_Another_Smart_RenamerTests.m
+//  Just Another Smart RenamerTests
+//
+//  Created by Администратор on 12/4/12.
+//  Copyright (c) 2012 Nope. All rights reserved.
+//
+
+#import "Just_Another_Smart_RenamerTests.h"
+
+@implementation Just_Another_Smart_RenamerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Just Another Smart RenamerTests");
+}
+
+@end
