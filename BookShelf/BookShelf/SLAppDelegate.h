8646e4bd03445a0701fc19e6ba7281cef49f932d
slebedev <sergey.al.lebedev@gmail.com>
2012-12-04 23:45:50 +0400

    I try!

--- BookShelf/BookShelf/SLAppDelegate.h
+++ BookShelf/BookShelf/SLAppDelegate.h
@@ -10,7 +10,7 @@
 
 @interface SLAppDelegate : NSObject <NSApplicationDelegate>
 
-@property (assign) IBOutlet NSWindow *window;
+
 
 @property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
 @property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
