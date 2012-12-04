f93418a124c2dfba527f2a2ae4e0a55d3ffeeca9
deron <mailderon@gmail.com>
2012-12-04 08:54:58 +0400

    FINAL
    FINAL

---
+++ myTasks/myTasks/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  myTasks
+//
+//  Created by deron on 04.12.12.
+//  Copyright (c) 2012 deron. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ myTasks/myTasksTests/myTasksTests.m
@@ -0,0 +1,32 @@
+//
+//  myTasksTests.m
+//  myTasksTests
+//
+//  Created by deron on 04.12.12.
+//  Copyright (c) 2012 deron. All rights reserved.
+//
+
+#import "myTasksTests.h"
+
+@implementation myTasksTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in myTasksTests");
+}
+
+@end
