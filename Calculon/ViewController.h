ddc3e1d5e7fd41dc261d9a8dd8d2f23d30aba486
Sachs <suxxwork@gmail.com>
2012-12-04 22:39:39 +0400

    first commit
    примерный вид

---
+++ Calculon/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Calculon
+//
+//  Created by Sachs on 04.12.12.
+//  Copyright (c) 2012 SachsHome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CalculonTests/CalculonTests.m
@@ -0,0 +1,32 @@
+//
+//  CalculonTests.m
+//  CalculonTests
+//
+//  Created by Sachs on 04.12.12.
+//  Copyright (c) 2012 SachsHome. All rights reserved.
+//
+
+#import "CalculonTests.h"
+
+@implementation CalculonTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CalculonTests");
+}
+
+@end
