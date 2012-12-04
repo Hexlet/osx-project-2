e8fe2a01e9d819a8d518d8d2c64122a7187c1507
Alex Pulichkin <pulichkin@gmail.com>
2012-12-04 15:59:09 +0400

    Final commit
    Добавлен проект с xib файлом и описание в Readne.

---
+++ KBMonitoring/KBMonitoring/KB_AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  KB_AppDelegate.m
+//  KBMonitoring
+//
+//  Created by Alex Pulichkin on 04.12.12.
+//  Copyright (c) 2012 Alex Pulichkin. All rights reserved.
+//
+
+#import "KB_AppDelegate.h"
+
+@implementation KB_AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ KBMonitoring/KBMonitoring/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  KBMonitoring
+//
+//  Created by Alex Pulichkin on 04.12.12.
+//  Copyright (c) 2012 Alex Pulichkin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
