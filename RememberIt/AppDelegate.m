9ab481cd9c36ec90ae5d69e150a6dbd4f11fc064
TheRightWay <root.right.way@gmail.com>
2012-12-04 15:48:16 +0400

    FINAL

---
+++ RememberIt/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  RememberIt
+//
+//  Created by Администратор on 12/4/12.
+//  Copyright (c) 2012 RightWay. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ RememberItTests/RememberItTests.m
@@ -0,0 +1,32 @@
+//
+//  RememberItTests.m
+//  RememberItTests
+//
+//  Created by Администратор on 12/4/12.
+//  Copyright (c) 2012 RightWay. All rights reserved.
+//
+
+#import "RememberItTests.h"
+
+@implementation RememberItTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in RememberItTests");
+}
+
+@end
