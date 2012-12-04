227dad29e565ef14e326662faf8ff71d75794eea
Roman Tsymbluk <roman.tsymbaluk@wemaketraffic.com>
2012-12-04 13:16:41 +0200

    v.0.0.1

---
+++ Radio-T/Radio-T/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Radio-T
+//
+//  Created by Roman Tsymbalyuk on 12/4/12.
+//  Copyright (c) 2012 Fruitcode. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
