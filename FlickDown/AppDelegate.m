1f673110bd61f846647214cdeba0ed7bdbf6297e
Pavel Dmitriev <p.dmitriev@nimble.com>
2012-12-04 10:14:24 +0200

    Initial Commit

---
+++ FlickDown/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  FlickDown
+//
+//  Created by Paul Dmitryev on 04.12.12.
+//  Copyright (c) 2012 Paul Dmitryev. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ FlickDownTests/FlickDownTests.m
@@ -0,0 +1,32 @@
+//
+//  FlickDownTests.m
+//  FlickDownTests
+//
+//  Created by Paul Dmitryev on 04.12.12.
+//  Copyright (c) 2012 Paul Dmitryev. All rights reserved.
+//
+
+#import "FlickDownTests.h"
+
+@implementation FlickDownTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in FlickDownTests");
+}
+
+@end
