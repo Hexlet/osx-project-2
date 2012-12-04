b52b6b73d04db26007295e5ca6091f166073434c
Vladimir Kovalev <derats@gmail.com>
2012-12-04 11:43:00 +0400

    FINAL

---
+++ search/search/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  search
+//
+//  Created by Администратор on 12/4/12.
+//  Copyright (c) 2012 Администратор. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
