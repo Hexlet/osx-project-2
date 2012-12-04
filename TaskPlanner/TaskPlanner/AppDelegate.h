458c44bc9bcea9ed0680b324f308c602b6f425aa
kodiers <kodiers@gmail.com>
2012-12-04 23:50:04 +0400

    Final

---
+++ TaskPlanner/TaskPlanner/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TaskPlanner
+//
+//  Created by Viktor Yamchinov on 12/4/12.
+//  Copyright (c) 2012 Viktor Yamchinov. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ TaskPlanner/TaskPlanner/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TaskPlanner
+//
+//  Created by Viktor Yamchinov on 12/4/12.
+//  Copyright (c) 2012 Viktor Yamchinov. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
