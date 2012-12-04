8a835de7e4dfa6d5d35493c09d0a8aee49d8692d
bazoon <vith77@gmail.com>
2012-12-04 20:45:01 +0600

    initial

---
+++ LoanCalculatorTests/LoanCalculatorTests.h
@@ -0,0 +1,13 @@
+//
+//  LoanCalculatorTests.h
+//  LoanCalculatorTests
+//
+//  Created by VITALIY NESTERENKO on 04.12.12.
+//  Copyright (c) 2012 VITALIY NESTERENKO. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface LoanCalculatorTests : SenTestCase
+
+@end
