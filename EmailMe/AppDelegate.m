b8c9a2411e361ae0b99cf2585de4a215b74a77dc
AndrewYermolovich <andrew.yermolovich.github@gmail.com>
2012-12-04 11:06:33 +0300

    my project

---
+++ EmailMe/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  EmailMe
+//
+//  Created by Андрей Ермолович on 04.12.12.
+//  Copyright (c) 2012 Андрей Ермолович. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
