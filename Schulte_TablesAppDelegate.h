14fb1a07acca59b91cda8b9cc16f07b56708f99c
Alexander Alexeychuk <kikudjiro@gmail.com>
2012-12-04 22:23:34 +0200

    FINAL

---
+++ Schulte_TablesAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  Schulte_TablesAppDelegate.h
+//  Schulte Tables
+//
+//  Created by Alex on 04.12.12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Schulte_TablesAppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
