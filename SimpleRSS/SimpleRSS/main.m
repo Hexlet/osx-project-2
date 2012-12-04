2223c76eb4c358685e1e3f9220cd42be4d2989e5
Dmitry Katsubo <dk@iMac-Dmitry.local>
2012-12-04 19:41:36 +0300

    Project 2: Idea and user interface

---
+++ SimpleRSS/SimpleRSS/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  SimpleRSS
+//
+//  Created by Dmitry Katsubo on 04.12.12.
+//  Copyright (c) 2012 Dmitry Katsubo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
