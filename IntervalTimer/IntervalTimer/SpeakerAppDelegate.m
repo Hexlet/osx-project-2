5ed02974f9ed98b9002370c1b7f4aa20b5e6f461
igolovin01 <igolovin01@gmail.com>
2012-12-04 07:08:01 +0200

    commit my assigment

---
+++ IntervalTimer/IntervalTimer/SpeakerAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  SpeakerAppDelegate.m
+//  IntervalTimer
+//
+//  Created by INOKENTII GOLOVIN on 01.12.12.
+//  Copyright (c) 2012 INOKENTII GOLOVIN. All rights reserved.
+//
+
+#import "SpeakerAppDelegate.h"
+
+@implementation SpeakerAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ IntervalTimer/IntervalTimer/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  IntervalTimer
+//
+//  Created by INOKENTII GOLOVIN on 01.12.12.
+//  Copyright (c) 2012 INOKENTII GOLOVIN. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
