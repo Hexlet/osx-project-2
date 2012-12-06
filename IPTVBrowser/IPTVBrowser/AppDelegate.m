bf48ecdb991802a9feccbe5ee192dd20825ee484
4pcbr <me@4pcbr.com>
2012-12-06 14:54:10 +0400

    создано окно приложения

---
+++ IPTVBrowser/IPTVBrowser/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  IPTVBrowser
+//
+//  Created by 4pcbr on 12/6/12.
+//  Copyright (c) 2012 whitebox. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
