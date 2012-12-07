e54375340ed76f1e2206323b6457f2c1bc0331af
Oleg <olegto@neti.ee>
2012-12-07 21:54:30 +0200

    Final
    My final

---
+++ SNMP Mon/SNMP Mon/AppDelegate.m
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.m
+//  SNMP Mon
+//
+//  Created by Oleg on 07.12.12.
+//  Copyright (c) 2012 Oleg. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+    NSBeep();
+}
+
+@end
---
+++ SNMP Mon/SNMP Mon/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  SNMP Mon
+//
+//  Created by Oleg on 07.12.12.
+//  Copyright (c) 2012 Oleg. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
