82cbf543934afa29f4aef62aa55152deb4b44fd5
olya-d <inremajor@gmail.com>
2012-12-07 15:49:57 +0400

    Watching commit

---
+++ Watching/WatchingAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WatchingAppDelegate.h
+//  Watching
+//
+//  Created by Olga Andreyeva on 12/7/12.
+//  Copyright (c) 2012 Olga Andreyeva. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface WatchingAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Watching/WatchingAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  WatchingAppDelegate.m
+//  Watching
+//
+//  Created by Olga Andreyeva on 12/7/12.
+//  Copyright (c) 2012 Olga Andreyeva. All rights reserved.
+//
+
+#import "WatchingAppDelegate.h"
+
+@implementation WatchingAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
