5ec4f13c491e20ab9c5a012c8eb4dfb30015c0f0
smallkot <kostya_po@mail.ru>
2012-12-07 19:13:14 +0800

    Final
    интерфейс будет еще дорабатываться

---
+++ vk_player/vk_player/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  vk_player
+//
+//  Created by Smallkot on 07.12.12.
+//  Copyright (c) 2012 Smallkot. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
