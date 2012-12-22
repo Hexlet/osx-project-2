15e6de333c772ca0075ce4fe44b31491c2eb0e6f
Maxim <pms@nm.ru>
2012-12-23 01:08:41 +0400

    First .xib interface

---
+++ SimpleTimer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SimpleTimer
+//
+//  Created by Maxim on 22.12.12.
+//  Copyright (c) 2012 AppleKiller. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ SimpleTimer/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  SimpleTimer
+//
+//  Created by Maxim on 22.12.12.
+//  Copyright (c) 2012 AppleKiller. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
